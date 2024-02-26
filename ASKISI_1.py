8cf7cfb4df1c88ff8b4c2b89e41a1b3b1faabfd5
giorgis <panagiwtisnik@gmail.com>
2024-02-26 12:28:12 +0200

    added show server and show cookies

--- ASKISI_1.py
+++ ASKISI_1.py
@@ -13,5 +13,22 @@ def more(text):
 url = 'http://python.org/'  # προσδιορισμός του url
 
 with requests.get(url) as response:  # το αντικείμενο response
-    html = response.text
-    more(html)
+#    html = response.text
+#    more(html)
+
+
+    print(f"Website headers are {url} \n , {response.headers} \n\n")
+
+    server = response.headers.get('Server')
+
+    if server:
+        print(f"the server is {server}")
+    else:
+        print(f"no server found")
+    
+    cookies = response.headers.get('Set-Cookie')
+
+    if cookies:
+        print(f"the cookies are {cookies}")
+    else:
+        print("no cookies found")
